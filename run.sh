39555c8a516f4cd63e72713cfc2e0d650d9be51f
Max Kranker <maxkranker@protonmail.com>
2025-07-31 16:32:05 -0400

    Fixed the run.sh file being able to run as a symlink from somewhere else by creating a $DIR variable

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-source ./venv/bin/activate
-python main.py
+
+DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+
+source $DIR/venv/bin/activate
+python $DIR/main.py
 deactivate
 read -p "Press Enter to close the window..."
